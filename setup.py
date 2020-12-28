df7cfb4ba1efbe3e74e03e68d601573f7307f07e
Thierry Florac <thierry.florac@onf.fr>
2020-12-28 11:51:03 +0100

    Version 1.2.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ DOCS = os.path.join(os.path.dirname(__file__),
 README = os.path.join(DOCS, 'README.txt')
 HISTORY = os.path.join(DOCS, 'HISTORY.txt')
 
-version = '1.2.0'
+version = '1.2.1'
 long_description = open(README).read() + '\n\n' + open(HISTORY).read()
 
 tests_require = [
@@ -71,7 +71,7 @@ setup(name='pyams_security',
           'pyams_i18n',
           'pyams_mail',
           'pyams_site',
-          'pyams_utils',
+          'pyams_utils >= 1.3.6',
           'pyramid',
           'pyramid_chameleon',
           'pyramid_mailer',
